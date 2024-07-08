210f02ecfdb82f3409a49dab618fefc153207750
SUMIT DAS <das684704@gmail.com>
2024-07-08 10:10:54 +0530

    User Auth

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CustomButton from "./CustomButton";
+import TextInput from "./TextInput";
+import SignUp from "./SignUp";
 
-export {Navbar,Footer,CustomButton};
+export {Navbar,Footer,CustomButton,TextInput,SignUp};
