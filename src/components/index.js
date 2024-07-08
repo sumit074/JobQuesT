1a2b80a2e40b1e43c983c60cf15adb746635a1cf
SUMIT DAS <das684704@gmail.com>
2024-07-08 11:00:44 +0530

    Header

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,23 @@ import Footer from "./Footer";
 import CustomButton from "./CustomButton";
 import TextInput from "./TextInput";
 import SignUp from "./SignUp";
+import Header from "./Header";
+import ListBox from "./ListBox";
+import JobCard from "./JobCard";
+import Loading from "./Loading";
+import CompanyCard from "./CompanyCard";
+import JobTypes from "./JobTypes";
 
-export {Navbar,Footer,CustomButton,TextInput,SignUp};
+export {
+  Navbar,
+  Footer,
+  CustomButton,
+  TextInput,
+  SignUp,
+  Header,
+  ListBox,
+  JobCard,
+  Loading,
+  CompanyCard,
+  JobTypes,
+};
